3275382d0f25719dafed83291a155f61ecc43999
EngineeringAwesome <robomower9508@gmail.com>
2015-09-30 19:42:13 -0400

    driveForward probably isn't necessary

--- RobotMain/Drivetrain.cpp
+++ RobotMain/Drivetrain.cpp
@@ -1,1 +1,15 @@
 #include "Drivetrain.h"
+
+/** Drives the robot with the given speed and turn rate.
+  * Values range between -1 and 1 where 1 specifies full
+  * speed forward, or full speed turning left.
+  * @param speed Speed that the robot should travel at
+  * @param turnRate Rate that the robot should turn at
+  **/
+void Drivetrain::drive(float speed, float turnRate) {
+
+}
+
+void swingTurn(float turnRate);
+void pointTurn(float turnRate);
+void stop();
--- RobotMain/Drivetrain.h
+++ RobotMain/Drivetrain.h
@@ -7,16 +7,15 @@
 class Drivetrain {
 public:
 	void drive(float speed, float turnRate);
-	void driveForward(float speed);
 	void swingTurn(float turnRate);
 	void pointTurn(float turnRate);
 	void stop();
-	Servo leftMotor;
-	Servo rightMotor;
 
 private:
 	static const int LEFT_MOTOR_PIN = 6;
 	static const int RIGHT_MOTOR_PIN = 5;
+	Servo leftMotor;
+	Servo rightMotor;
 };
 
 #endif
