16dd93f3440630834ad0b8a242a3c7397866b583
hejohnson <hejohnson@wpi.edu>
2015-09-30 19:46:58 -0400

    Changed multipliers
    Changed the multipliers for each sensor to be the distance from the
    center of the robot in 1/16ths of an inch

--- RobotMain/LineTracker.h
+++ RobotMain/LineTracker.h
@@ -7,10 +7,10 @@ public:
 	LineTracker();
 	float lineError();
 private:
-	static const int m1 = 20;
-	static const int m2 = 12;
-	static const int m3 = 8;
-	static const int m4 = 4;
+	static const int m1 = 21; // In 1/16ths of an inch
+	static const int m2 = 15;
+	static const int m3 = 9;
+	static const int m4 = 3;
 
 };
 
