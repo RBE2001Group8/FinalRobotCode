08533eb81edc0077df66c9bc1d5ac56d6a5dfc70
EngineeringAwesome <robomower9508@gmail.com>
2015-10-01 16:10:19 -0400

    Removed debugging Serial prints

--- RobotMain/Drivetrain.cpp
+++ RobotMain/Drivetrain.cpp
@@ -22,21 +22,17 @@ void Drivetrain::initialize() {
   * @param turnRate Rate that the robot should turn at
   **/
 void Drivetrain::drive(float speed, float turnRate) {
+	//Calculate the values for each motor based on speed and turn rate.  Note right is reversed
 	int leftValue = MICROSECONDS_STOP + (MICROSECONDS_MAX_RANGE*speed) - ((float)MICROSECONDS_MAX_RANGE*turnRate);
 	int rightValue = MICROSECONDS_STOP - (MICROSECONDS_MAX_RANGE*speed) - ((float)MICROSECONDS_MAX_RANGE*turnRate);
-	Serial.print("Left value: ");
-	Serial.print(leftValue);
-	Serial.print(", Right value: ");
-	Serial.println(rightValue);
+
+	//Constrain the values to valid ranges for the servo
 	leftValue = constrain(leftValue, MICROSECONDS_FULL_REVERSE, MICROSECONDS_FULL_FORWARD);
 	rightValue = constrain(rightValue, MICROSECONDS_FULL_REVERSE, MICROSECONDS_FULL_FORWARD);
+
+	//Write the values to the motors
 	leftMotor.writeMicroseconds(leftValue);
 	rightMotor.writeMicroseconds(rightValue);
-	Serial.print("Left value: ");
-	Serial.print(leftValue);
-	Serial.print(", Right value: ");
-	Serial.println(rightValue);
-
 }
 
 /** Moves the robot through a swing turn at the given
