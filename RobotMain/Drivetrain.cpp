34fb884c8919bdfbcef5ca114ac15d19159b3cb5
EngineeringAwesome <robomower9508@gmail.com>
2015-09-30 20:47:00 -0400

    Implements driving functionality
    Fully implemented drive, swingturn, pointturn, stop, and initialize.
    Code has been tested and verified on the robot

--- RobotMain/Drivetrain.cpp
+++ RobotMain/Drivetrain.cpp
@@ -1,5 +1,20 @@
 #include "Drivetrain.h"
 
+
+
+/** Constructor
+  * Intentionally does not initialize the drivetrain
+  **/
+Drivetrain::Drivetrain() {
+
+}
+/** Initializes the drivetrain, and sets up the motor port
+  **/
+void Drivetrain::initialize() {
+	leftMotor.attach(LEFT_MOTOR_PIN);
+	rightMotor.attach(RIGHT_MOTOR_PIN);
+}
+
 /** Drives the robot with the given speed and turn rate.
   * Values range between -1 and 1 where 1 specifies full
   * speed forward, or full speed turning left.
@@ -7,9 +22,50 @@
   * @param turnRate Rate that the robot should turn at
   **/
 void Drivetrain::drive(float speed, float turnRate) {
-
+	int leftValue = MICROSECONDS_STOP + (MICROSECONDS_MAX_RANGE*speed) - (MICROSECONDS_MAX_RANGE*turnRate);
+	int rightValue = MICROSECONDS_STOP - (MICROSECONDS_MAX_RANGE*speed) - (MICROSECONDS_MAX_RANGE*turnRate);
+	leftValue = constrain(leftValue, MICROSECONDS_FULL_FORWARD, MICROSECONDS_FULL_REVERSE);
+	rightValue = constrain(rightValue, MICROSECONDS_FULL_FORWARD, MICROSECONDS_FULL_REVERSE);
+	leftMotor.writeMicroseconds(leftValue);
+	rightMotor.writeMicroseconds(rightValue);
 }
 
-void swingTurn(float turnRate);
-void pointTurn(float turnRate);
-void stop();
+/** Moves the robot through a swing turn at the given
+  * rate. Where one wheel is stopped, and the other moves
+  * at the specified rate.  Values range from -1 to 1 where
+  * 1 is full speed swing turn left.
+  * @note Only swings forward
+  * @param turnRate Speed that the robot should turn at
+  **/
+void Drivetrain::swingTurn(float turnRate) {
+	if(turnRate == 0.0) {
+		//Don't turn
+		stop();
+	} else if(turnRate > 0) {
+		//turn left
+		leftMotor.writeMicroseconds(MICROSECONDS_STOP);
+		rightMotor.writeMicroseconds(MICROSECONDS_STOP + (MICROSECONDS_MAX_RANGE*turnRate));
+	} else {
+		//turn right
+		leftMotor.writeMicroseconds(MICROSECONDS_STOP + (MICROSECONDS_MAX_RANGE*turnRate));
+		rightMotor.writeMicroseconds(MICROSECONDS_STOP);
+	}
+}
+
+/** Moves the robot through a point turn at the given
+  * rate, where the wheels rotate in opposite directions
+  * at the same speed.  Values range from -1 to 1 where
+  * 1 is full speed turning left.
+  * @param turnRate Speed that the robot should turn at
+  **/
+void Drivetrain::pointTurn(float turnRate){
+	leftMotor.writeMicroseconds(MICROSECONDS_STOP + (MICROSECONDS_MAX_RANGE*turnRate));
+	rightMotor.writeMicroseconds(MICROSECONDS_STOP + (MICROSECONDS_MAX_RANGE*turnRate));
+}
+
+/** Stops the drivetrain.
+  **/
+void Drivetrain::stop() {
+	leftMotor.writeMicroseconds(MICROSECONDS_STOP);
+	rightMotor.writeMicroseconds(MICROSECONDS_STOP);
+}
--- RobotMain/Drivetrain.h
+++ RobotMain/Drivetrain.h
@@ -6,6 +6,8 @@
 
 class Drivetrain {
 public:
+	Drivetrain();
+	void initialize();
 	void drive(float speed, float turnRate);
 	void swingTurn(float turnRate);
 	void pointTurn(float turnRate);
@@ -14,6 +16,11 @@ public:
 private:
 	static const int LEFT_MOTOR_PIN = 6;
 	static const int RIGHT_MOTOR_PIN = 5;
+	static const int MICROSECONDS_STOP = 1500;
+	static const int MICROSECONDS_FULL_FORWARD = 2000;
+	static const int MICROSECONDS_FULL_REVERSE = 1000;
+	static const int MICROSECONDS_MAX_RANGE = MICROSECONDS_FULL_FORWARD - MICROSECONDS_STOP;
+	/* Right Motor is reversed (2000 usec moves the motor in reverse) */
 	Servo leftMotor;
 	Servo rightMotor;
 };
