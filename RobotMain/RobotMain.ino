18082f0c5171a4e7a3c8cc26f1619217ba4fb37a
hejohnson <hejohnson@wpi.edu>
2015-10-02 15:35:21 -0400

    Raise Arm, Line follow, drive roller

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -6,18 +6,29 @@
 #include "BlinkLED.h"
 #include "Robot.h"
 #include "Scheduler.h"
+#include "LineFollowToSwitch.h"
+#include "MoveArm.h"
+#include "Roller.h"
 
 //#TODO This got moved to the robot class.  Will be removed soon
 Scheduler* scheduler = Scheduler::getInstance();
 
+Roller roller;
+
 /** Code to initialize the robot **/
 void setup() {
-
+	scheduler->addCommand(new MoveArm(680));
+	scheduler->addCommand(new LineFollowToSwitch(0.5));
+	roller.initialize();
 }
 
 /** Code to iteratively operate the robot **/
 void loop() {
 	scheduler->run();
+
+	if (!digitalRead(12)) {
+		roller.spit();
+	}
 
 }
 
