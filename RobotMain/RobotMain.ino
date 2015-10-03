7b38ca96a84e78e5fb0432a021f09d37d1a9cfb2
engineeringawesome <robomower9508@gmail.com>
2015-10-03 17:20:53 -0400

    Clears out testing code

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -4,30 +4,15 @@
    * @author Hans Johnson
    **/
 #include "BlinkLED.h"
-#include "RobotConstants.h"
 #include "Robot.h"
 #include "Scheduler.h"
-
-#include "UserButton.h"
-#include "WaitUntilPressed.h"
 
 //#TODO This got moved to the robot class.  Will be removed soon
 Scheduler* scheduler = Scheduler::getInstance();
 
-UserButton myButton(USER_BUTTON_PIN);
-
-
 /** Code to initialize the robot **/
 void setup() {
-	Serial.begin(115200);
-	myButton.initialize();
 
-	//Waits until the button is pressed and then blinks the LED 4 times
-	scheduler->addCommand(new WaitUntilPressed(myButton));
-	scheduler->addCommand(new BlinkLED(100));
-	scheduler->addCommand(new BlinkLED(100));
-	scheduler->addCommand(new BlinkLED(100));
-	scheduler->addCommand(new BlinkLED(100));
 }
 
 /** Code to iteratively operate the robot **/
