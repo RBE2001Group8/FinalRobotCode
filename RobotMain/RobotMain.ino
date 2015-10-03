daf659857642c8783373a8aa2fb5669ed7dcea63
hejohnson <hejohnson@wpi.edu>
2015-10-03 15:31:08 -0400

    Testing for all commands so far
    Robot stops driving after LineFollowToCross, after all the other
    commands finish running, the roller will run if the reactor switch is
    pressed

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -6,9 +6,14 @@
 #include "BlinkLED.h"
 #include "Robot.h"
 #include "Scheduler.h"
+#include "LineFollowToCrossLine.h"
 #include "LineFollowToSwitch.h"
 #include "MoveArm.h"
-#include "Roller.h"
+#include "RollerSpit.h"
+#include "RollerSuck.h"
+#include "Drive.h"
+#include "SwingTurn.h"
+#include "PointTurn.h"
 
 //#TODO This got moved to the robot class.  Will be removed soon
 Scheduler* scheduler = Scheduler::getInstance();
@@ -16,19 +21,21 @@ Scheduler* scheduler = Scheduler::getInstance();
 Roller roller;
 
 /** Code to initialize the robot **/
-void setup() {
+void setup() {	
+	scheduler->addCommand(new MoveArm(1015));
+	scheduler->addCommand(new RollerSuck(2000));
 	scheduler->addCommand(new MoveArm(680));
-	scheduler->addCommand(new LineFollowToSwitch(0.5));
-	roller.initialize();
+	scheduler->addCommand(new Drive(-0.75, 0.2, 750));
+	scheduler->addCommand(new PointTurn(-1.0, 1200));
+	scheduler->addCommand(new LineFollowToCrossLine(0.5));
+	scheduler->addCommand(new Drive(-0.75, 0.2, 100));
+	scheduler->addCommand(new SwingTurn(1.0, 1600));
+	scheduler->addCommand(new LineFollowToSwitch(0.75));
+	scheduler->addCommand(new RollerSpit(1500));
 }
 
 /** Code to iteratively operate the robot **/
 void loop() {
 	scheduler->run();
-
-	if (!digitalRead(12)) {
-		roller.spit();
-	}
-
 }
 
