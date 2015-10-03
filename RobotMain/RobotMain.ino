c06ca2ef1dcbe24b6a5e1d99f42464e393993f34
engineeringawesome <robomower9508@gmail.com>
2015-10-03 17:18:40 -0400

    Verifies functionality of the WaitForButton command

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -9,27 +9,30 @@
 #include "Scheduler.h"
 
 #include "UserButton.h"
+#include "WaitUntilPressed.h"
 
 //#TODO This got moved to the robot class.  Will be removed soon
 Scheduler* scheduler = Scheduler::getInstance();
 
 UserButton myButton(USER_BUTTON_PIN);
 
+
 /** Code to initialize the robot **/
 void setup() {
 	Serial.begin(115200);
 	myButton.initialize();
+
+	//Waits until the button is pressed and then blinks the LED 4 times
+	scheduler->addCommand(new WaitUntilPressed(myButton));
+	scheduler->addCommand(new BlinkLED(100));
+	scheduler->addCommand(new BlinkLED(100));
+	scheduler->addCommand(new BlinkLED(100));
+	scheduler->addCommand(new BlinkLED(100));
 }
 
 /** Code to iteratively operate the robot **/
 void loop() {
 	scheduler->run();
-	if(myButton.isPressed()) {
-		Serial.println("Button is pressed");
-	} else {
-		Serial.println("Button is not pressed");
-	}
-	delay(250);
 
 }
 
