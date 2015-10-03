25db47fcc7f7f853aabe6b081de9609cc2d2a846
engineeringawesome <robomower9508@gmail.com>
2015-10-03 16:46:51 -0400

    initialized UserButton Class

---
+++ RobotMain/UserButton.h
@@ -0,0 +1,14 @@
+#ifndef USERBUTTON_H
+#define USERBUTTON_H
+
+#include <Arduino.h>
+/*
+class UserButton {
+public:
+	UserButton(int pin);
+	bool isPressed();
+private:
+	const int BUTTON_PIN;
+};  */
+
+#endif
