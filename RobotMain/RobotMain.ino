c38cdc5961ab37a0fe8cf447b5f1b318e02f8c96
engineeringawesome <robomower9508@gmail.com>
2015-10-03 17:11:30 -0400

    Validates functionality of the UserButton class

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -4,20 +4,32 @@
    * @author Hans Johnson
    **/
 #include "BlinkLED.h"
+#include "RobotConstants.h"
 #include "Robot.h"
 #include "Scheduler.h"
+
+#include "UserButton.h"
 
 //#TODO This got moved to the robot class.  Will be removed soon
 Scheduler* scheduler = Scheduler::getInstance();
 
+UserButton myButton(USER_BUTTON_PIN);
+
 /** Code to initialize the robot **/
 void setup() {
-
+	Serial.begin(115200);
+	myButton.initialize();
 }
 
 /** Code to iteratively operate the robot **/
 void loop() {
 	scheduler->run();
+	if(myButton.isPressed()) {
+		Serial.println("Button is pressed");
+	} else {
+		Serial.println("Button is not pressed");
+	}
+	delay(250);
 
 }
 
