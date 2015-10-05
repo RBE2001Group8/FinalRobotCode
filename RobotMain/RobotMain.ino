58285aed27c6781f8a5f1541eab466145ab850f3
hejohnson <hejohnson@wpi.edu>
2015-10-05 17:27:44 -0400

    Added commands to ensure fuel rod is placed properly, and fully removed from storage

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -32,29 +32,44 @@ void setup() {
 	myButton.initialize();
 	//Serial.begin(9600);
 	scheduler->addCommand(new WaitUntilPressed(myButton));
+
 	scheduler->addCommand(new MoveArm(650));
 	scheduler->addCommand(new RollerSuck(1500));
-	scheduler->addCommand(new Drive(-0.25, 0.2, 200, myDrive));
+	scheduler->addCommand(new Drive(-0.25, 0.1, 200, myDrive));
 	scheduler->addCommand(new RollerSuck(1000));
 	scheduler->addCommand(new MoveArm(potDown+120));
+
 	scheduler->addCommand(new RollerSpit(250));
-	scheduler->addCommand(new MoveArm(potDown+270));
-	scheduler->addCommand(new Drive(-0.75, 0.2, 750, myDrive));
-	scheduler->addCommand(new PointTurn(-0.5, 2000, myDrive));
-	scheduler->addCommand(new LineFollowToCrossLine(0.5, myDrive));
-	scheduler->addCommand(new Drive(0.75, 0.2, 200, myDrive));
-	scheduler->addCommand(new LineFollowToCrossLine(0.5, myDrive));
-	scheduler->addCommand(new Drive(-0.75, 0.2, 200, myDrive));
+	scheduler->addCommand(new MoveArm(potDown+265));
+
+	scheduler->addCommand(new Drive(-0.5, 0.1, 1000, myDrive));
+	scheduler->addCommand(new PointTurn(-0.5, 2100, myDrive));
+
+	scheduler->addCommand(new LineFollowToCrossLine(0.5, myDrive)); // First line
+
+	scheduler->addCommand(new Drive(0.5, 0.1, 300, myDrive));
+	scheduler->addCommand(new LineFollowToCrossLine(0.5, myDrive)); // Second line
+
+	scheduler->addCommand(new Drive(-0.5, 0.1, 300, myDrive)); // Back up to center on lone
+
 	scheduler->addCommand(new SwingTurn(1.0, 1100, myDrive));
 	scheduler->addCommand(new LineFollowToSwitch(0.75, myDrive));
-	scheduler->addCommand(new RollerSpit(2000));
-	// scheduler->addCommand(new Drive(-0.25, 0.2, 200, myDrive));
-	// scheduler->addCommand(new MoveArm(potDown+320));
-	// scheduler->addCommand(new RollerSpit(1000));
-	scheduler->addCommand(new Drive(-0.75, 0.2, 500, myDrive));
+
+	scheduler->addCommand(new RollerSpit(1500));
+	scheduler->addCommand(new Drive(-0.25, 0.1, 350, myDrive));
+	scheduler->addCommand(new RollerSpit(500));
+
+	scheduler->addCommand(new Drive(-0.75, 0.1, 500, myDrive));
 	scheduler->addCommand(new PointTurn(-0.5, 2000, myDrive));
+
 	scheduler->addCommand(new LineFollowToSwitch(0.75, myDrive));
+
 	scheduler->addCommand(new RollerSuck(1500));
+
+	scheduler->addCommand(new Drive(-0.5, 0.1, 750, myDrive));
+
+	scheduler->addCommand(new RollerSuck(1500));
+
 }
 
 /** Code to iteratively operate the robot **/
