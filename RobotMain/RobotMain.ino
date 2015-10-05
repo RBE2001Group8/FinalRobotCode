7b9f1448f035436d7cfc52f4d7a925be74277963
hejohnson <hejohnson@wpi.edu>
2015-10-05 17:11:20 -0400

    Updated with new pot positions

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -25,19 +25,20 @@ Scheduler* scheduler = Scheduler::getInstance();
 Roller roller;
 Drivetrain myDrive(LEFT_MOTOR_PIN, RIGHT_MOTOR_PIN);
 UserButton myButton(USER_BUTTON_PIN);
+const int potDown = 690;
 
 /** Code to initialize the robot **/
 void setup() {	
 	myButton.initialize();
 	//Serial.begin(9600);
 	scheduler->addCommand(new WaitUntilPressed(myButton));
-	scheduler->addCommand(new MoveArm(1010));
+	scheduler->addCommand(new MoveArm(650));
 	scheduler->addCommand(new RollerSuck(1500));
 	scheduler->addCommand(new Drive(-0.25, 0.2, 200, myDrive));
 	scheduler->addCommand(new RollerSuck(1000));
-	scheduler->addCommand(new MoveArm(900));
+	scheduler->addCommand(new MoveArm(potDown+120));
 	scheduler->addCommand(new RollerSpit(250));
-	scheduler->addCommand(new MoveArm(675));
+	scheduler->addCommand(new MoveArm(potDown+270));
 	scheduler->addCommand(new Drive(-0.75, 0.2, 750, myDrive));
 	scheduler->addCommand(new PointTurn(-0.5, 2000, myDrive));
 	scheduler->addCommand(new LineFollowToCrossLine(0.5, myDrive));
@@ -46,10 +47,10 @@ void setup() {
 	scheduler->addCommand(new Drive(-0.75, 0.2, 200, myDrive));
 	scheduler->addCommand(new SwingTurn(1.0, 1100, myDrive));
 	scheduler->addCommand(new LineFollowToSwitch(0.75, myDrive));
-	scheduler->addCommand(new RollerSpit(1000));
-	scheduler->addCommand(new Drive(-0.25, 0.2, 200, myDrive));
-	scheduler->addCommand(new MoveArm(665));
-	scheduler->addCommand(new RollerSpit(1000));
+	scheduler->addCommand(new RollerSpit(2000));
+	// scheduler->addCommand(new Drive(-0.25, 0.2, 200, myDrive));
+	// scheduler->addCommand(new MoveArm(potDown+320));
+	// scheduler->addCommand(new RollerSpit(1000));
 	scheduler->addCommand(new Drive(-0.75, 0.2, 500, myDrive));
 	scheduler->addCommand(new PointTurn(-0.5, 2000, myDrive));
 	scheduler->addCommand(new LineFollowToSwitch(0.75, myDrive));
