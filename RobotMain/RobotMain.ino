c985833864d061cf6e68c0005d09191badf5fc2b
hejohnson <hejohnson@wpi.edu>
2015-10-05 19:46:47 -0400

    Places collected rod in second slot, picks up rod from slot directly across

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -9,6 +9,7 @@
 #include "Scheduler.h"
 #include "LineFollowToCrossLine.h"
 #include "LineFollowToSwitch.h"
+#include "PointTurnToLine.h"
 #include "MoveArm.h"
 #include "RollerSpit.h"
 #include "RollerSuck.h"
@@ -25,50 +26,61 @@ Scheduler* scheduler = Scheduler::getInstance();
 Roller roller;
 Drivetrain myDrive(LEFT_MOTOR_PIN, RIGHT_MOTOR_PIN);
 UserButton myButton(USER_BUTTON_PIN);
+Roller myRoller;
 const int potDown = 670;
 
 /** Code to initialize the robot **/
 void setup() {	
 	myButton.initialize();
+	myRoller.initialize();
 	//Serial.begin(9600);
 	scheduler->addCommand(new WaitUntilPressed(myButton));
 
 	scheduler->addCommand(new MoveArm(potDown));
-	scheduler->addCommand(new RollerSuck(1500));
-	scheduler->addCommand(new Drive(-0.25, 0.1, 200, myDrive));
-	scheduler->addCommand(new RollerSuck(1000));
-	scheduler->addCommand(new MoveArm(potDown+120));
+	scheduler->addCommand(new RollerSuck(1500, myRoller));
+	scheduler->addCommand(new Drive(-0.25, 0.0, 200, myDrive));
+	scheduler->addCommand(new RollerSuck(1000, myRoller));
+	scheduler->addCommand(new MoveArm(potDown+90));
 
-	scheduler->addCommand(new RollerSpit(250));
+	scheduler->addCommand(new RollerSpit(250, myRoller));
 	scheduler->addCommand(new MoveArm(potDown+290));
 
-	scheduler->addCommand(new Drive(-0.5, 0.1, 1000, myDrive));
-	scheduler->addCommand(new PointTurn(-0.5, 2100, myDrive));
+	scheduler->addCommand(new Drive(-0.5, 0.0, 1000, myDrive));
+	scheduler->addCommand(new PointTurn(-0.5, 1000, myDrive));
+	scheduler->addCommand(new PointTurnToLine(-0.5, myDrive));
 
 	scheduler->addCommand(new LineFollowToCrossLine(0.5, myDrive)); // First line
 
 	scheduler->addCommand(new Drive(0.5, 0.1, 300, myDrive));
 	scheduler->addCommand(new LineFollowToCrossLine(0.5, myDrive)); // Second line
 
-	scheduler->addCommand(new Drive(-0.5, 0.1, 200, myDrive)); // Back up to center on lone
+	scheduler->addCommand(new Drive(-0.5, 0.0, 200, myDrive)); // Back up to center on lone
 
 	scheduler->addCommand(new SwingTurn(1.0, 1100, myDrive));
 	scheduler->addCommand(new LineFollowToSwitch(0.75, myDrive));
 
-	scheduler->addCommand(new RollerSpit(1500));
-	scheduler->addCommand(new Drive(-0.25, 0.1, 350, myDrive));
-	scheduler->addCommand(new RollerSpit(500));
+	scheduler->addCommand(new RollerSpit(1500, myRoller));
+	scheduler->addCommand(new Drive(-0.25, 0.0, 350, myDrive));
+	scheduler->addCommand(new RollerSpit(500, myRoller));
 
-	scheduler->addCommand(new Drive(-0.75, 0.1, 500, myDrive));
-	scheduler->addCommand(new PointTurn(-0.5, 2000, myDrive));
+	scheduler->addCommand(new Drive(-0.75, 0.0, 500, myDrive));
+	scheduler->addCommand(new PointTurn(-0.375, 2300, myDrive));
 
 	scheduler->addCommand(new LineFollowToSwitch(0.75, myDrive));
 
-	scheduler->addCommand(new RollerSuck(1000));
+	scheduler->addCommand(new RollerSuck(1000, myRoller));
+	scheduler->addCommand(new Drive(-0.5, 0.0, 750, myDrive));
+	scheduler->addCommand(new RollerSuck(1000, myRoller));
 
-	scheduler->addCommand(new Drive(-0.5, 0.1, 750, myDrive));
+	scheduler->addCommand(new MoveArm(potDown+90));
+	scheduler->addCommand(new RollerSpit(250, myRoller));
 
-	scheduler->addCommand(new RollerSuck(1000));
+	scheduler->addCommand(new Drive(-0.5, 0.0, 750, myDrive));
+	scheduler->addCommand(new SwingTurn(-1.0, 1100, myDrive));
+	scheduler->addCommand(new LineFollowToSwitch(0.75, myDrive));
+	scheduler->addCommand(new MoveArm(potDown));
+	scheduler->addCommand(new RollerSpit(750, myRoller));
+	scheduler->addCommand(new MoveArm(potDown+100));
 
 }
 
