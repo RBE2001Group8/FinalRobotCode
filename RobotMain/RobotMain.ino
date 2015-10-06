2aaed0f7141522c2daabc3fe112f5dac8d793da3
engineeringawesome <robomower9508@gmail.com>
2015-10-06 11:22:04 -0400

    Verifies parallel command functionality

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -5,30 +5,39 @@
    **/
 #include "BlinkLED.h"
 #include "Robot.h"
+#include "RobotConstants.h"
 #include "Scheduler.h"
 #include "LineFollowToSwitch.h"
 #include "MoveArm.h"
 #include "Roller.h"
+#include "WaitUntilPressed.h"
+#include "UserButton.h"
+#include "PersistentBlink.h"
 
 //#TODO This got moved to the robot class.  Will be removed soon
 Scheduler* scheduler = Scheduler::getInstance();
 
 Roller roller;
+UserButton myButton(USER_BUTTON_PIN);
 
 /** Code to initialize the robot **/
 void setup() {
-	scheduler->addCommand(new MoveArm(680));
-	scheduler->addCommand(new LineFollowToSwitch(0.5));
+	myButton.initialize();
+	Serial.begin(115200);
+	scheduler->addParallelCommand(new PersistentBlink(100));
+	scheduler->addSequentialCommand(new WaitUntilPressed(myButton));
+	scheduler->addSequentialCommand(new BlinkLED(500));
+	scheduler->addSequentialCommand(new BlinkLED(500));
+	scheduler->addSequentialCommand(new BlinkLED(500));
+	scheduler->addSequentialCommand(new BlinkLED(500));
+
 	roller.initialize();
 }
 
 /** Code to iteratively operate the robot **/
 void loop() {
 	scheduler->run();
-
-	if (!digitalRead(12)) {
-		roller.spit();
-	}
+	scheduler->printCommands();
 
 }
 
