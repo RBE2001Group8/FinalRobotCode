4700c3793d2a02025e93922190785bd620b9ef96
hejohnson <hejohnson@wpi.edu>
2015-10-06 19:17:03 -0400

    Currently unused commands
    May or may not work

--- RobotMain/SwingTurnToLine.h
+++ RobotMain/SwingTurnToLine.h
@@ -1,14 +1,14 @@
-#ifndef POINTTURN2LINE_H
-#define POINTTURN2LINE_H
+#ifndef SWINGTURN2LINE_H
+#define SWINGTURN2LINE_H
 
 #include "Command.h"
 #include <Arduino.h>
 #include "Drivetrain.h"
 #include "LineTracker.h"
 
-class PointTurnToLine : public Command {
+class SwingTurnToLine : public Command {
 public:
-	PointTurnToLine(float turn, Drivetrain drive);
+	SwingTurnToLine(float turn, Drivetrain drive);
 	void initialize();
 	void execute();
 	void end();
