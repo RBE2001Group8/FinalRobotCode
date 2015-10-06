6978e9a46165a83386657259f217b45b99c6f374
hejohnson <hejohnson@wpi.edu>
2015-10-06 19:15:20 -0400

    Testing for these commands

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -19,11 +19,20 @@
 #include "Drivetrain.h"
 #include "WaitUntilPressed.h"
 #include "UserButton.h"
-#include "LineFollowToNearest.h"
+#include "LineFollowOverLines.h"
+#include "DriveToRearLine.h"
+#include "SwingTurnToLine.h"
+#include "SwingTurnToPosition.h"
+#include "DriveAndSquareOnLine.h"
+#include "PointTurnToPosition.h"
+#include "TurnToPosition.h"
 
 const int potDown = 670;
 
 char storageBitMap = 10;
+
+int currentPos = 2;
+int newPos = 1;
 
 Scheduler* scheduler = Scheduler::getInstance();
 
@@ -35,7 +44,15 @@ void setup() {
 
 	//Serial.begin(9600);
 	scheduler->addCommand(new WaitUntilPressed(curie->button));
-    scheduler->addCommand(new LineFollowToNearest(0.5, curie->drivetrain, storageBitMap, false));
+	scheduler->addCommand(new DriveToRearLine(-0.375, 0.07, curie->drivetrain));
+	scheduler->addCommand(new Drive(-0.375, 0.07, 250, curie->drivetrain));
+	scheduler->addCommand(new PointTurnToPosition(0.375, 2000, curie->drivetrain, &currentPos, &newPos));
+	scheduler->addCommand(new Drive(-0.375, 0.07, 250, curie->drivetrain));
+	scheduler->addCommand(new DriveAndSquareOnLine(0.375, 0.07, curie->drivetrain));
+	scheduler->addCommand(new Drive(-0.375, 0.07, 400, curie->drivetrain));
+    scheduler->addCommand(new LineFollowOverLines(0.5, curie->drivetrain, &currentPos, &newPos));
+    scheduler->addCommand(new TurnToPosition(0.5, 2200, curie->drivetrain, &currentPos, &newPos));
+    scheduler->addCommand(new LineFollowToSwitch(0.375, curie->drivetrain));
 }
 
 /** Code to iteratively operate the robot **/
