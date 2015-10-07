59df6b8d2564ccf40597ad51fa2888637634f853
hejohnson <hejohnson@wpi.edu>
2015-10-07 08:48:52 -0400

    Verified that it works for all position setups

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -28,8 +28,8 @@ const int potDown = 665;
 
 char storageBitMap = 10;
 
-int dropoffPos = 1;
-int pickupPos = 3;
+int dropoffPos = 2;
+int pickupPos = 1;
 
 Scheduler* scheduler = Scheduler::getInstance();
 
@@ -70,7 +70,7 @@ void setup() {
 	scheduler->addSequentialCommand(new DriveAndSquareOnLine(0.375, 0.07, curie->drivetrain));
 	scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 600, curie->drivetrain));
     scheduler->addSequentialCommand(new LineFollowOverLines(0.5, curie->drivetrain, &dropoffPos, &pickupPos));
-    scheduler->addSequentialCommand(new TurnToPosition(0.375, 2000, curie->drivetrain, &dropoffPos, &pickupPos));
+    scheduler->addSequentialCommand(new TurnToPosition(0.4, 2000, curie->drivetrain, &dropoffPos, &pickupPos));
     scheduler->addSequentialCommand(new LineFollowToSwitch(0.5, curie->drivetrain));
 
     scheduler->addSequentialCommand(new RollerSuck(1000, curie->roller));
