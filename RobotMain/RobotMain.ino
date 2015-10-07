6aadaa66b80c48599f3fe810632669dd76aaa1b5
hejohnson <hejohnson@wpi.edu>
2015-10-06 20:12:35 -0400

    Updated to support sequential vs parallel commands

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -39,57 +39,57 @@ void setup() {
 	curie->initializeSubsystems();
 
 	//Serial.begin(9600);
-	scheduler->addCommand(new WaitUntilPressed(curie->button));
+	scheduler->addSequentialCommand(new WaitUntilPressed(curie->button));
 
-	scheduler->addCommand(new MoveArm(potDown));
-	scheduler->addCommand(new RollerSuck(1500, curie->roller));
-	scheduler->addCommand(new Drive(-0.25, 0.0, 200, curie->drivetrain));
-	scheduler->addCommand(new RollerSuck(1000, curie->roller));
-	scheduler->addCommand(new MoveArm(potDown+90));
+	scheduler->addSequentialCommand(new MoveArm(potDown));
+	scheduler->addSequentialCommand(new RollerSuck(1500, curie->roller));
+	scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 200, curie->drivetrain));
+	scheduler->addSequentialCommand(new RollerSuck(1000, curie->roller));
+	scheduler->addSequentialCommand(new MoveArm(potDown+90));
 
-	scheduler->addCommand(new RollerSpit(250, curie->roller));
-	scheduler->addCommand(new MoveArm(potDown+290));
+	scheduler->addSequentialCommand(new RollerSpit(250, curie->roller));
+	scheduler->addSequentialCommand(new MoveArm(potDown+290));
 
-	scheduler->addCommand(new Drive(-0.5, 0.0, 1000, curie->drivetrain));
-	scheduler->addCommand(new PointTurn(-0.5, 1000, curie->drivetrain));
-	scheduler->addCommand(new PointTurnToLine(-0.5, curie->drivetrain));
-	scheduler->addCommand(new LineFollowOverLines(0.5, curie->drivetrain, 0, &dropoffPos));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 1000, curie->drivetrain));
+	scheduler->addSequentialCommand(new PointTurn(-0.5, 1000, curie->drivetrain));
+	scheduler->addSequentialCommand(new PointTurnToLine(-0.5, curie->drivetrain));
+	scheduler->addSequentialCommand(new LineFollowOverLines(0.5, curie->drivetrain, 0, &dropoffPos));
 
-	scheduler->addCommand(new Drive(-0.5, 0.0, 200, curie->drivetrain)); // Back up to center on line
-	scheduler->addCommand(new SwingTurn(-1.0, 1100, curie->drivetrain));
-	scheduler->addCommand(new LineFollowToSwitch(0.75, curie->drivetrain));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 200, curie->drivetrain)); // Back up to center on line
+	scheduler->addSequentialCommand(new SwingTurn(-1.0, 1100, curie->drivetrain));
+	scheduler->addSequentialCommand(new LineFollowToSwitch(0.75, curie->drivetrain));
 
-	scheduler->addCommand(new RollerSpit(1500, curie->roller));
-	scheduler->addCommand(new Drive(-0.25, 0.0, 350, curie->drivetrain));
-	scheduler->addCommand(new RollerSpit(500, curie->roller)); // Ensure rod is fully placed
+	scheduler->addSequentialCommand(new RollerSpit(1500, curie->roller));
+	scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 350, curie->drivetrain));
+	scheduler->addSequentialCommand(new RollerSpit(500, curie->roller)); // Ensure rod is fully placed
 
-	scheduler->addCommand(new DriveToRearLine(-0.375, 0.07, curie->drivetrain));
-	scheduler->addCommand(new Drive(-0.375, 0.07, 250, curie->drivetrain));
-	scheduler->addCommand(new PointTurnToPosition(0.375, 2000, curie->drivetrain, &dropoffPos, &pickupPos));
-	scheduler->addCommand(new Drive(-0.375, 0.07, 250, curie->drivetrain));
-	scheduler->addCommand(new DriveAndSquareOnLine(0.375, 0.07, curie->drivetrain));
-	scheduler->addCommand(new Drive(-0.375, 0.07, 400, curie->drivetrain));
-    scheduler->addCommand(new LineFollowOverLines(0.5, curie->drivetrain, &dropoffPos, &pickupPos));
-    scheduler->addCommand(new TurnToPosition(0.5, 2200, curie->drivetrain, &dropoffPos, &pickupPos));
-    scheduler->addCommand(new LineFollowToSwitch(0.375, curie->drivetrain));
+	scheduler->addSequentialCommand(new DriveToRearLine(-0.375, 0.07, curie->drivetrain));
+	scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 250, curie->drivetrain));
+	scheduler->addSequentialCommand(new PointTurnToPosition(0.375, 2000, curie->drivetrain, &dropoffPos, &pickupPos));
+	scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 250, curie->drivetrain));
+	scheduler->addSequentialCommand(new DriveAndSquareOnLine(0.375, 0.07, curie->drivetrain));
+	scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 400, curie->drivetrain));
+    scheduler->addSequentialCommand(new LineFollowOverLines(0.5, curie->drivetrain, &dropoffPos, &pickupPos));
+    scheduler->addSequentialCommand(new TurnToPosition(0.5, 2200, curie->drivetrain, &dropoffPos, &pickupPos));
+    scheduler->addSequentialCommand(new LineFollowToSwitch(0.375, curie->drivetrain));
 
-    scheduler->addCommand(new RollerSuck(1000, curie->roller));
-	scheduler->addCommand(new Drive(-0.5, 0.0, 750, curie->drivetrain));
-	scheduler->addCommand(new RollerSuck(1000, curie->roller));
+    scheduler->addSequentialCommand(new RollerSuck(1000, curie->roller));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 750, curie->drivetrain));
+	scheduler->addSequentialCommand(new RollerSuck(1000, curie->roller));
 
-	scheduler->addCommand(new MoveArm(potDown+90));
+	scheduler->addSequentialCommand(new MoveArm(potDown+90));
 
-	scheduler->addCommand(new DriveToRearLine(-0.375, 0.07, curie->drivetrain));
-	scheduler->addCommand(new Drive(-0.375, 0.07, 250, curie->drivetrain));
-	scheduler->addCommand(new PointTurn(0.5, 1600, curie->drivetrain));
+	scheduler->addSequentialCommand(new DriveToRearLine(-0.375, 0.07, curie->drivetrain));
+	scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 250, curie->drivetrain));
+	scheduler->addSequentialCommand(new PointTurn(0.5, 1600, curie->drivetrain));
 
-	scheduler->addCommand(new LineFollowToSwitch(0.75, curie->drivetrain));
+	scheduler->addSequentialCommand(new LineFollowToSwitch(0.75, curie->drivetrain));
 
-	scheduler->addCommand(new MoveArm(potDown));
-	scheduler->addCommand(new RollerSpit(1250, curie->roller));
-	scheduler->addCommand(new Drive(-0.25, 0.0, 200, curie->drivetrain));
-	scheduler->addCommand(new RollerSpit(1250, curie->roller));
-	scheduler->addCommand(new MoveArm(potDown+100));
+	scheduler->addSequentialCommand(new MoveArm(potDown));
+	scheduler->addSequentialCommand(new RollerSpit(1250, curie->roller));
+	scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 200, curie->drivetrain));
+	scheduler->addSequentialCommand(new RollerSpit(1250, curie->roller));
+	scheduler->addSequentialCommand(new MoveArm(potDown+100));
 
 }
 
