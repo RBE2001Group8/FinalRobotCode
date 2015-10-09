d29189b1778ab6df3f752f4ed8d5ab9cdb7c056f
hejohnson <hejohnson@wpi.edu>
2015-10-09 18:32:26 -0400

    Rewriting with new turning strategy

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -27,6 +27,8 @@
 #include "PersistentSendRadAlerts.h"
 #include "PersistentGetPackets.h"
 #include "PersistentWarnRadiation.h"
+#include "LineFollowToCrossLine.h"
+#include "SwingTurnToLine.h"
 
 const int potDown = 665;
 
@@ -71,37 +73,37 @@ void setup() {
 	scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NONE));
 
 	//scheduler->addSequentialCommand(new DriveToRearLine(-0.375, 0.07));
-	scheduler->addSequentialCommand(new Drive(-0.5, 0.07, 750));
-	scheduler->addSequentialCommand(new PointTurn(-0.5, -1.0));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.07, 500));
+	scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
 	scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
-	// scheduler->addSequentialCommand(new PointTurnToPosition(0.375, 1700, true));
-	// scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 550));
-	// scheduler->addSequentialCommand(new DriveAndSquareOnLine(0.375, 0.07));
-	// scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 600));
- //    scheduler->addSequentialCommand(new LineFollowOverLines(0.5, true));
- //    scheduler->addSequentialCommand(new TurnToPosition(0.4, 2000, true));
- //    scheduler->addSequentialCommand(new LineFollowToSwitch(0.5));
+	scheduler->addSequentialCommand(new LineFollowToCrossLine(0.5));
+	scheduler->addSequentialCommand(new TurnToSupplyDirection(0.375, true));
+    scheduler->addSequentialCommand(new LineFollowOverLines(0.5, true));
+    scheduler->addSequentialCommand(new TurnToPosition(0.4, true));
+ 	scheduler->addSequentialCommand(new LineFollowToSwitch(0.5));
 
- //    scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NEW));
- //    scheduler->addSequentialCommand(new RollerSuck(1000));
-	// scheduler->addParallelCommand(new Drive(-0.5, 0.0, 750));
-	// scheduler->addSequentialCommand(new RollerSuck(1000));
+    scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NEW));
+    scheduler->addSequentialCommand(new RollerSuck(1000));
+	scheduler->addParallelCommand(new Drive(-0.5, 0.0, 750));
+	scheduler->addSequentialCommand(new RollerSuck(1000));
 
-	// scheduler->addParallelCommand(new MoveArm(potDown+90));
+	scheduler->addParallelCommand(new MoveArm(potDown+90));
 
-	// scheduler->addSequentialCommand(new DriveToRearLine(-0.375, 0.07));
-	// scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 250));
-	// scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.07, 500));
+	scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
+	scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
+	scheduler->addSequentialCommand(new LineFollowToCrossLine(0.5));
+	scheduler->addSequentialCommand(new SwingTurnToLine(-0.5));
 
-	// scheduler->addSequentialCommand(new LineFollowToSwitch(0.75));
+	scheduler->addSequentialCommand(new LineFollowToSwitch(0.75));
 
-	// scheduler->addSequentialCommand(new MoveArm(potDown));
-	// scheduler->addSequentialCommand(new RollerSpit(1250));
-	// scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 200));
-	// scheduler->addSequentialCommand(new RollerSpit(1250));
-	// scheduler->addParallelCommand(new RollerSpit(500));
-	// scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NONE));
-	// scheduler->addSequentialCommand(new MoveArm(potDown+100));
+	scheduler->addSequentialCommand(new MoveArm(potDown));
+	scheduler->addSequentialCommand(new RollerSpit(1250));
+	scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 200));
+	scheduler->addSequentialCommand(new RollerSpit(1250));
+	scheduler->addParallelCommand(new RollerSpit(500));
+	scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NONE));
+	scheduler->addSequentialCommand(new MoveArm(potDown+100));
 
 	// /////////////////////////// SIDE 2 //////////////////////////////// 
 	// scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 1000));
