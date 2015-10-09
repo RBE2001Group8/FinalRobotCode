afb78722d2f2352589a5ff43dadf34f7b3357af3
hejohnson <hejohnson@wpi.edu>
2015-10-09 18:32:06 -0400

    Added command
    Turns from central line in the correct direction to the nearest supply
    tube

---
+++ RobotMain/TurnToSupplyDirection.h
@@ -0,0 +1,24 @@
+#ifndef TURN2SD_H
+#define TURN2SD_H
+
+#include "Command.h"
+#include <Arduino.h>
+#include "Robot.h"
+
+class TurnToSupplyDirection : public Command {
+public:
+	TurnToSupplyDirection(float turn, bool sideA);
+	void initialize();
+	void execute();
+	void end();
+	bool isFinished();
+private:
+	float _turn;
+	Robot* curie;
+	int deltaPos;
+	bool prevCenter = true;
+	bool _sideA;
+	char _bitmask;
+};
+
+#endif
