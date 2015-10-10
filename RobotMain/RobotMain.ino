dd58adc14b0309ebc19ea11e158b8f85785b580b
hejohnson <hejohnson@wpi.edu>
2015-10-10 00:25:34 -0400

    Renabled the second half of the field

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -30,9 +30,7 @@
 #include "LineFollowToCrossLine.h"
 #include "SwingTurnToLine.h"
 #include "TurnToSupplyDirection.h"
-
-int potDown = 665;
-
+#include "EnsureRodInserted.h"
 
 Scheduler* scheduler = Scheduler::getInstance();
 
@@ -40,7 +38,7 @@ Robot* curie = Robot::getInstance();
 
 /** Code to initialize the robot **/
 void setup() {	
-	potDown = 665;
+	int potDown = 665;
 	Serial.begin(115200);
 	curie->initializeSubsystems();
 
@@ -73,9 +71,9 @@ void setup() {
 	scheduler->addSequentialCommand(new RollerSpit(1000)); // Ensure rod is fully placed
 	scheduler->addParallelCommand(new RollerSpit(1000));
 	scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 350));
+	scheduler->addSequentialCommand(new EnsureRodInserted(0.5, SIDE_A));
 	scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NONE));
 
-	//scheduler->addSequentialCommand(new DriveToRearLine(-0.375, 0.07));
 	scheduler->addSequentialCommand(new Drive(-0.5, 0.07, 500));
 	scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
 	scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
@@ -84,24 +82,22 @@ void setup() {
 
 	scheduler->addSequentialCommand(new TurnToSupplyDirection(0.375, SIDE_A));
     scheduler->addSequentialCommand(new LineFollowOverLines(0.5, SIDE_A));
-    //scheduler->addSequentialCommand(new Drive(0.5, 0.0, 100));
     scheduler->addSequentialCommand(new TurnToPosition(0.4, SIDE_A));
 
   	scheduler->addSequentialCommand(new LineFollowToSwitch(0.5));
 
     scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NEW));
     scheduler->addSequentialCommand(new RollerSuck(1000));
-	scheduler->addParallelCommand(new Drive(-0.5, 0.0, 500));
+	scheduler->addParallelCommand(new Drive(-0.5, 0.0, 550));
 	scheduler->addSequentialCommand(new RollerSuck(1000));
 
 	scheduler->addParallelCommand(new MoveArm(potDown+90));
 
-	//scheduler->addSequentialCommand(new Drive(-0.5, 0.07, 500));
 	scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
 	scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
 	scheduler->addSequentialCommand(new LineFollowToCrossLine(0.5));
 	scheduler->addSequentialCommand(new SwingTurnToLine(0.5));
-	scheduler->addSequentialCommand(new SwingTurn(0.5, 500));
+	scheduler->addSequentialCommand(new SwingTurn(0.5, 750));
 
 	scheduler->addSequentialCommand(new LineFollowToSwitch(0.75));
 
@@ -114,64 +110,70 @@ void setup() {
 	scheduler->addSequentialCommand(new MoveArm(potDown+100));
 
 	// /////////////////////////// SIDE 2 //////////////////////////////// 
-	// scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 1000));
-	// scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
-	// scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
-	// scheduler->addSequentialCommand(new LineFollowToSwitch(0.5));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 1000));
+	scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
+	scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
+	scheduler->addSequentialCommand(new LineFollowToSwitch(0.5));
 
-	// scheduler->addSequentialCommand(new MoveArm(potDown));
-	// scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_SPENT));
-	// scheduler->addSequentialCommand(new RollerSuck(1500));
-	// scheduler->addParallelCommand(new Drive(-0.25, 0.0, 200));
-	// scheduler->addSequentialCommand(new RollerSuck(1000));
+	scheduler->addSequentialCommand(new MoveArm(potDown));
+	scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_SPENT));
+	scheduler->addSequentialCommand(new RollerSuck(1500));
+	scheduler->addParallelCommand(new Drive(-0.25, 0.0, 200));
+	scheduler->addSequentialCommand(new RollerSuck(1000));
 
-	// scheduler->addParallelCommand(new RollerSpit(250));
-	// scheduler->addSequentialCommand(new MoveArm(potDown+290));
+	scheduler->addParallelCommand(new RollerSpit(250));
+	scheduler->addSequentialCommand(new MoveArm(potDown+290));
 
-	// scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 1000));
-	// scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
-	// scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
-	// scheduler->addSequentialCommand(new LineFollowToStorage(0.5, SIDE_B));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 1000));
+	scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
+	scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
+	scheduler->addSequentialCommand(new LineFollowToStorage(0.5, SIDE_B));
 
-	// scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 200)); // Back up to center on line
-	// scheduler->addSequentialCommand(new SwingTurn(-1.0, 950));
-	// scheduler->addSequentialCommand(new LineFollowToSwitch(0.75));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 200)); // Back up to center on line
+	//#TODO Make this swing turn to line
+	scheduler->addSequentialCommand(new SwingTurn(-1.0, 950));
+	scheduler->addSequentialCommand(new LineFollowToSwitch(0.75));
 
-	// scheduler->addSequentialCommand(new RollerSpit(1000)); // Ensure rod is fully placed
-	// scheduler->addParallelCommand(new RollerSpit(1000));
-	// scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 350));
-	// scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NONE));
+	scheduler->addSequentialCommand(new RollerSpit(1000)); // Ensure rod is fully placed
+	scheduler->addParallelCommand(new RollerSpit(1000));
+	scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 350));
+	scheduler->addSequentialCommand(new EnsureRodInserted(0.5, SIDE_B));
+	scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NONE));
 
-	// scheduler->addSequentialCommand(new DriveToRearLine(-0.375, 0.07));
-	// scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 250));
-	// scheduler->addSequentialCommand(new PointTurnToPosition(0.375, 1700, SIDE_B));
-	// scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 550));
-	// scheduler->addSequentialCommand(new DriveAndSquareOnLine(0.375, 0.07));
-	// scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 600));
- //    scheduler->addSequentialCommand(new LineFollowOverLines(0.5, SIDE_B));
- //    scheduler->addSequentialCommand(new TurnToPosition(0.4, 2000, SIDE_B));
- //    scheduler->addSequentialCommand(new LineFollowToSwitch(0.5));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.07, 500));
+	scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
+	scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
+	scheduler->addSequentialCommand(new LineFollowToCrossLine(0.5));
+	scheduler->addSequentialCommand(new Drive(-0.5, 0.0, 200));
 
- //    scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NEW));
- //    scheduler->addSequentialCommand(new RollerSuck(1000));
-	// scheduler->addParallelCommand(new Drive(-0.5, 0.0, 750));
-	// scheduler->addSequentialCommand(new RollerSuck(1000));
+	scheduler->addSequentialCommand(new TurnToSupplyDirection(0.375, SIDE_B));
+    scheduler->addSequentialCommand(new LineFollowOverLines(0.5, SIDE_B));
+    scheduler->addSequentialCommand(new TurnToPosition(0.4, SIDE_B));
 
-	// scheduler->addParallelCommand(new MoveArm(potDown+90));
+  	scheduler->addSequentialCommand(new LineFollowToSwitch(0.5));
 
-	// scheduler->addSequentialCommand(new DriveToRearLine(-0.375, 0.07));
-	// scheduler->addSequentialCommand(new Drive(-0.375, 0.07, 250));
-	// scheduler->addSequentialCommand(new PointTurn(0.5, 1700));
+    scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NEW));
+    scheduler->addSequentialCommand(new RollerSuck(1000));
+	scheduler->addParallelCommand(new Drive(-0.5, 0.0, 550));
+	scheduler->addSequentialCommand(new RollerSuck(1000));
 
-	// scheduler->addSequentialCommand(new LineFollowToSwitch(0.75));
+	scheduler->addParallelCommand(new MoveArm(potDown+90));
 
-	// scheduler->addSequentialCommand(new MoveArm(potDown));
-	// scheduler->addSequentialCommand(new RollerSpit(1250));
-	// scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 200));
-	// scheduler->addSequentialCommand(new RollerSpit(1250));
-	// scheduler->addParallelCommand(new RollerSpit(500));
-	// scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NONE));
-	// scheduler->addSequentialCommand(new MoveArm(potDown+100));
+	scheduler->addSequentialCommand(new PointTurn(-0.5, 1000));
+	scheduler->addSequentialCommand(new PointTurnToLine(-0.5));
+	scheduler->addSequentialCommand(new LineFollowToCrossLine(0.5));
+	scheduler->addSequentialCommand(new SwingTurnToLine(-0.5));
+	scheduler->addSequentialCommand(new SwingTurn(-0.5, 750));
+
+	scheduler->addSequentialCommand(new LineFollowToSwitch(0.75));
+
+	scheduler->addSequentialCommand(new MoveArm(potDown));
+	scheduler->addSequentialCommand(new RollerSpit(1250));
+	scheduler->addSequentialCommand(new Drive(-0.25, 0.0, 200));
+	scheduler->addSequentialCommand(new RollerSpit(1250));
+	scheduler->addParallelCommand(new RollerSpit(500));
+	scheduler->addParallelCommand(new SetRadiationLevel(curie, RAD_LEVEL_NONE));
+	scheduler->addSequentialCommand(new MoveArm(potDown+100));
 
 
 	//curie->setRadLevel(RAD_LEVEL_NEW);
