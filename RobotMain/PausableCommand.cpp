3d2972e34da4517d8d878c6f0f8bb9ca8afedbc1
engineeringawesome <robomower9508@gmail.com>
2015-10-13 10:50:42 -0400

    implements functionality

--- RobotMain/PausableCommand.cpp
+++ RobotMain/PausableCommand.cpp
@@ -1,2 +1,55 @@
 
 #include "PausableCommand.h"
+
+/** Constructor **/
+PausableCommand::PausableCommand() : Command() {
+	robotPauser = RobotPauser::getInstance();
+}
+
+
+/** Get the time from the start of the command
+  * @return time since the command has started in milliseconds
+  **/
+unsigned long PausableCommand::getTime() {
+	return getPauseMillis() - this->startTime;
+}
+
+/* Background code for initialization */
+void PausableCommand::_initialize() {
+	running = true;
+	startTime = getPauseMillis();
+}
+
+/** Execute an iteration of the command
+  **/
+bool PausableCommand::cycle() {
+	bool finished = false;
+	bool isNowPaused = robotPauser.isPaused(); 
+
+	if(!initialized) {
+		initialize();
+		_initialize();
+		initialized = true;
+	} else if (isFinished()) {
+		finished = true;
+		end();
+		_end();
+	} else {
+		if(isNowPaused && (isNowPaused != wasPaused)) {
+			//Robot is paused, but wasn't last time
+			onPause();
+		} else if(!isNowPaused && (isNowPaused != wasPaused)) {
+			//Robot is resumed, but wasn't last time
+			onResume();
+		} else if(!isNowPaused) {
+			//Robot is not paused, and was not paused last time
+			_execute();
+			execute();
+		} else {
+			//Robot is paused.  Do nothing
+		}
+	}
+
+	wasPaused = isNowPaused;
+	return finished;
+}
--- RobotMain/PausableCommand.h
+++ RobotMain/PausableCommand.h
@@ -1,28 +1,43 @@
 /** Extension of the Command class that can be paused
   * at any given time.  When paused, the internal timer
-  * does not increment
+  * does not increment.  When switching between paused or resumed
+  * the respective onPause and onResume methods are calld.  When
+  * paused, the execute method does not get called
   **/
 
 #ifndef PAUSABLECOMMAND_H
 #define PAUSABLECOMMAND_H
 
 #include "Command.h"
+#include "RobotPauser.h"
 #include "PausableTimer.h"
+
+//#TODO could switch to register a listener to RobotPauser
+//So that onPause was called exactly when RobotPauser.pauseRobot was called
+//RobotPauser would then need a list of "pausable" objects to call onPause and onResume methods
 
 class PausableCommand : public Command {
 public:
+	/* Constructors */
+	PausableCommand();
+	PausableCommand(const String name);
+
 	virtual void initialize() = 0;
 	virtual void execute() = 0;
 	virtual void end() = 0;
 	virtual bool isFinished() = 0;
+	/* Event to run when robot is paused */
 	virtual void onPause() = 0;
+	/* Event to run when robot is resumed */
 	virtual void onResume() = 0;
 
+	/* Override methods from the Command class to use PausableTimer */
 	virtual void _initialize();
-	virtual void _execute();
-	virtual void _end();
+	virtual unsigned long getTime();
+	virtual bool cycle();
 private:
 	bool wasPaused;
+	RobotPauser* robotPauser;
 };
 
 #endif
